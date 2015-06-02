ef726d5bdf54d0f5d24a5af5d9d87e9baad42632
Mike <mike@mikera.net>
2015-06-02 20:37:21 +0100

    more tests

--- src/test/clj/clojure/core/matrix/test_complex.clj
+++ src/test/clj/clojure/core/matrix/test_complex.clj
@@ -9,3 +9,11 @@
     (is (== 1 (cm/real c)))
     (is (== 2 (cm/imag c)))))
 
+(deftest test-complex-array
+  (let [ca (cm/complex-array [1 2] [3 4])]
+    (is (= [2] (m/shape ca)))
+    (is (m/equals [1 2] (cm/real ca)))
+    (is (m/equals [3 4] (cm/imag ca)))
+    (is (= (c/complex-number 1 3) (m/mget ca 0)))
+    (is (= (c/complex-number 2 4) (m/mget ca 1)))))
+
