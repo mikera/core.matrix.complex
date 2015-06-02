484d97e2b7ddc2093ac9c163305f877000fac018
Mike <mike@mikera.net>
2015-06-02 16:49:56 +0100

    add project.clj

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject core.matrix.complex "0.0.1-SNAPSHOT"
+  :description "Complex numerical arrays in Clojure, as an extension to core.matrix"
+  :url "https://github.com/mikera/core.matrix.complex"
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [net.mikera/core.matrix "0.34.0"]
+                 [complex "0.1.2"]
+                 [net.mikera/core.matrix.stats "0.5.0"]]
+
+  :profiles {:dev {:dependencies [[net.mikera/vectorz-clj "0.29.0"]]}}
+
+  :source-paths ["src/main/clj"]
+  :test-paths ["src/test/clj"])
